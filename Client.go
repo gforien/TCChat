0fec1d6f834d149772cedb8ad9ed8cb70f4e9564
Aeredren <romain.lebrun-thauront@insa-lyon.fr>
2018-12-24 12:06:41 -0400

    Fixing unapropriate unconsistent and barbaric use of spaces

--- Client.go
+++ Client.go
@@ -9,116 +9,116 @@ import (
 )
 
 var (
-    nickname string
-    userName []string
+	nickname string
+	userName []string
 
-    inputChan = make(chan string)
-    receiveChan = make(chan string)
-    conn net.Conn
-    connectionError error
+	inputChan = make(chan string)
+	receiveChan = make(chan string)
+	conn net.Conn
+	connectionError error
 )
 
 type Server struct {
-
+	name string
 }
 
 func main() {
 
-/*    // Enter server address
-    reader := bufio.NewReader(os.Stdin)
-    //fmt.Println("Enter the server adress and port (0.0.0.0:0000): ")
-    serverAdress, err := reader.ReadString('\n')
-    serverAdress = strings.TrimSuffix(serverAdress, "\n")
-    // Enter nickname
-    fmt.Println("\nEnter Your Nickname: ")
-    nickname, err := reader.ReadString('\n')
-    if err != nil {
-        panic(err)
-    }
-    // Connect to server
-    conn, err := net.Dial("tcp", serverAdress)
-    if err != nil {
-        panic(err)
-    }*/
+	/*    // Enter server address
+	reader := bufio.NewReader(os.Stdin)
+	//fmt.Println("Enter the server adress and port (0.0.0.0:0000): ")
+	serverAdress, err := reader.ReadString('\n')
+	serverAdress = strings.TrimSuffix(serverAdress, "\n")
+	// Enter nickname
+	fmt.Println("\nEnter Your Nickname: ")
+	nickname, err := reader.ReadString('\n')
+	if err != nil {
+	panic(err)
+}
+// Connect to server
+conn, err := net.Dial("tcp", serverAdress)
+if err != nil {
+panic(err)
+}*/
 
-    nickname = "Gabriel"
-    conn, connectionError = net.Dial("tcp", "127.0.0.1:2000")
-    if connectionError != nil {
-        panic(connectionError)
-    }
+nickname = "Gabriel"
+conn, connectionError = net.Dial("tcp", "127.0.0.1:2000")
+if connectionError != nil {
+	panic(connectionError)
+}
 
-    go getInput()
-    go getMsg()
+go getInput()
+go getMsg()
 
-    // Send first message
-    go func() {inputChan <- "TCCHAT_REGISTER\t"+"Gabriel"}()
+// Send first message
+go func() {inputChan <- "TCCHAT_REGISTER\t"+"Gabriel"}()
 
-    for {
-        select {
-        case input, ok := <-inputChan:
-            if !ok {
-                fmt.Println("Channel is closed !")
-                break
-            }
-            conn.Write([]byte(input + "\n"))
-            fmt.Println("WROTE :"+input)
-        case msg, ok := <-receiveChan:
-                        if !ok {
-                fmt.Println("Channel is closed !")
-                break
-            }
-			fmt.Println("NEW MSG :"+msg)
-        }
-    }
+for {
+	select {
+	case input, ok := <-inputChan:
+		if !ok {
+			fmt.Println("Channel is closed !")
+			break
+		}
+		conn.Write([]byte(input + "\n"))
+		fmt.Println(input)
+	case msg, ok := <-receiveChan:
+		if !ok {
+			fmt.Println("Channel is closed !")
+			break
+		}
+		fmt.Println("NEW MSG :"+msg)
+	}
+}
 }
 
 func getInput() {
-    reader := bufio.NewReader(os.Stdin)
-    for {
-        fmt.Print("Text to send: ")
-        text, err := reader.ReadString('\n')
-        text = strings.TrimSuffix(text, "\n")
-        if err != nil {
-            panic(err)
-        }
-        inputChan <- "TCCHAT_MESSAGE\t"+nickname+"\t"+text
-    }
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Text to send: ")
+		text, err := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+		if err != nil {
+			panic(err)
+		}
+		inputChan <- "TCCHAT_MESSAGE\t"+nickname+"\t"+text
+	}
 }
 
 func getMsg() {
-    var msgPieces []string
-    reader := bufio.NewReader(conn)
+	var msgPieces []string
+	reader := bufio.NewReader(conn)
 
-    for {
-        text, err := reader.ReadString('\n')
-        if err != nil {
-            fmt.Println("text = "+text)
-            panic(err)
-        }
-        text = strings.TrimSuffix(text, "\n")
+	for {
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("text = "+text)
+			panic(err)
+		}
+		text = strings.TrimSuffix(text, "\n")
 
-        msgPieces = strings.SplitN(strings.Split(text, "\n")[0], "\t", 3)
-        if len(msgPieces) < 2 || msgPieces[0] == "" || msgPieces[1] == ""{
-            fmt.Println(msgPieces)
-            panic("Error: Received message doesn't respect TC-Chat protocol.")
-        }
+		msgPieces = strings.SplitN(strings.Split(text, "\n")[0], "\t", 3)
+		if len(msgPieces) < 2 || msgPieces[0] == "" || msgPieces[1] == ""{
+			fmt.Println(msgPieces)
+			panic("Error: Received message doesn't respect TC-Chat protocol.")
+		}
 
-        switch msgPieces[0] {
+		switch msgPieces[0] {
 
-        case " ":
-            receiveChan <- "WELCOME"
+		case "TCCHAT_WELCOME":
+			receiveChan <- "WELCOME"
 
-        case "TCCHAT_USERIN":
-            receiveChan <- "USER IN"
+		case "TCCHAT_USERIN":
+			receiveChan <- "USER IN"
 
-        case "TCCHAT_USEROUT":
-            receiveChan <- "USER OUT"
+		case "TCCHAT_USEROUT":
+			receiveChan <- "USER OUT"
 
-        case "TCCHAT_BCAST":
-            if len(msgPieces) != 3 || msgPieces[2] == "" || len(msgPieces[2]) > 140 {
-                panic("Error: Received message doesn't respect TC-Chat protocol.")
-            }
-            receiveChan <- "USER "+msgPieces[1]+" SAID "+msgPieces[2]
-        }
-    }
+		case "TCCHAT_BCAST":
+			if len(msgPieces) != 3 || msgPieces[2] == "" || len(msgPieces[2]) > 140 {
+				panic("Error: Received message doesn't respect TC-Chat protocol.")
+			}
+			receiveChan <- "USER "+msgPieces[1]+" SAID "+msgPieces[2]
+		}
+	}
 }
--- Server.go
+++ Server.go
@@ -1,115 +1,115 @@
 package main
 
 import (
-    "net"
-    "fmt"
-    "bufio"
-    "strings"
-    )
+	"net"
+	"fmt"
+	"bufio"
+	"strings"
+)
 
 type Client struct {
-    conn net.Conn
-    name string
+	conn net.Conn
+	name string
 }
 
 var (
-    aConn = make([]Client, 1)
-    serverName = "TCChat Server"
-    // channels definition
-    inputChan = make(chan string)
-    connectChan = make(chan net.Conn)
-    msgChan = make(chan string)
-    broadcastChan = make(chan string)
+	aConn = make([]Client, 1)
+	serverName = "TCChat Server"
+	// channels definition
+	inputChan = make(chan string)
+	connectChan = make(chan net.Conn)
+	msgChan = make(chan string)
+	broadcastChan = make(chan string)
 )
 
 func main() {
 
-    fmt.Println("Launching server...")
-    listener, err := net.Listen("tcp", "127.0.0.1:2000")
-    if err != nil {
-        panic(err)
-    }
+	fmt.Println("Launching server...")
+	listener, err := net.Listen("tcp", "127.0.0.1:2000")
+	if err != nil {
+		panic(err)
+	}
 
-    go getConn(listener)
+	go getConn(listener)
 
-    // mainloop
-    for {
-        select {
+	// mainloop
+	for {
+		select {
 
-        case onConnection := <-connectChan:
-            fmt.Println("NEW CONN")
-            go sendMessage(Client{conn: onConnection, name : "undefined"}, "TCCHAT_WELCOME\t"+serverName)
-            go getMsg(onConnection, msgChan)
+		case onConnection := <-connectChan:
+			fmt.Println("NEW CONN")
+			go sendMessage(Client{conn: onConnection, name : "undefined"}, "TCCHAT_WELCOME\t"+serverName)
+			go getMsg(onConnection, msgChan)
 
-        case onMessage := <-msgChan:
-            fmt.Println("NEW MSG: ", onMessage)
+		case onMessage := <-msgChan:
+			fmt.Println("NEW MSG: ", onMessage)
 
-        case onBroadcast := <-broadcastChan:
-            for _, client := range aConn {
-                go sendMessage(client, onBroadcast)
-            }
+		case onBroadcast := <-broadcastChan:
+			for _, client := range aConn {
+				go sendMessage(client, onBroadcast)
+			}
 
-        case onInput := <-inputChan:
-            fmt.Println("NEW INPUT: ", onInput)
-        }
-    }
+		case onInput := <-inputChan:
+			fmt.Println("NEW INPUT: ", onInput)
+		}
+	}
 }
 
 func getMsg(conn net.Conn, msgChan chan string) {
-    var msgPieces []string
-    reader := bufio.NewReader(conn)
+	var msgPieces []string
+	reader := bufio.NewReader(conn)
 
-    for {
-        text, err := reader.ReadString('\n')
-        text = strings.TrimSuffix(text, "\n")
-        if err != nil {
-            panic(err)
-        }
+	for {
+		text, err := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+		if err != nil {
+			panic(err)
+		}
 
-        msgPieces = strings.SplitN(strings.Split(text, "\n")[0], "\t", 3)
-        if len(msgPieces) < 2 || msgPieces[0] == "" || msgPieces[1] == ""{
-            panic("Error: Received message doesn't respect TC-Chat protocol.")
-        }
+		msgPieces = strings.SplitN(strings.Split(text, "\n")[0], "\t", 3)
+		if len(msgPieces) < 2 || msgPieces[0] == "" || msgPieces[1] == ""{
+			panic("Error: Received message doesn't respect TC-Chat protocol.")
+		}
 
-        switch msgPieces[0] {
+		switch msgPieces[0] {
 
-        case "TCCHAT_REGISTER":
-            registerUser(conn, msgPieces[1]);
+		case "TCCHAT_REGISTER":
+			registerUser(conn, msgPieces[1]);
 
-        case "TCCHAT_MESSAGE":
-            if len(msgPieces) != 3 || msgPieces[2] == "" || len(msgPieces[2]) > 140 {
-                panic("Error: Received message doesn't respect TC-Chat protocol.")
-            }
-            broadcastChan <- "TCCHAT_BCAST\t"+msgPieces[1]+"\t"+msgPieces[2]
+		case "TCCHAT_MESSAGE":
+			if len(msgPieces) != 3 || msgPieces[2] == "" || len(msgPieces[2]) > 140 {
+				panic("Error: Received message doesn't respect TC-Chat protocol.")
+			}
+			broadcastChan <- "TCCHAT_BCAST\t"+msgPieces[1]+"\t"+msgPieces[2]
 
-        case "TCCHAT_DISCONNECT":
-            fmt.Println("TCCHAT_DISCONNECT")
-        }
-    }
+		case "TCCHAT_DISCONNECT":
+			fmt.Println("TCCHAT_DISCONNECT")
+		}
+	}
 }
 
 func getInput() {
 }
 
 func getConn(listener net.Listener) {
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            panic(err)
-        }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
 
-        connectChan <- conn
-    }
+		connectChan <- conn
+	}
 }
 
 /* Users/Messages - related function */
 
 func registerUser(connReceived net.Conn, nameReceived string) {
-    aConn = append(aConn, Client{conn: connReceived, name: nameReceived})
-    userInMessage := "TCCHAT_USERIN\t"+nameReceived
-    broadcastChan <- userInMessage
+	aConn = append(aConn, Client{conn: connReceived, name: nameReceived})
+	userInMessage := "TCCHAT_USERIN\t"+nameReceived
+	broadcastChan <- userInMessage
 }
 
 func sendMessage(client Client, msg string) {
-    client.conn.Write([]byte(msg + "\n"))
+	client.conn.Write([]byte(msg + "\n"))
 }
