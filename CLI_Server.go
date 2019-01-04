a51bf2ff3223220d77b657f8c3c6d54d1ea9dda0
Gabriel Forien <gforien@gmail.com>
2019-01-04 14:33:08 +0100

    Code restructuré en différentes fonctions, plus clair

--- CLI_Server.go
+++ CLI_Server.go
@@ -46,10 +46,11 @@ func getConn(serverName *string, listener net.Listener, userMap map[net.Conn]*Cl
     for {
         // upon receiving a new user :
         conn, err := listener.Accept()
-        fmt.Println("getConn():\tnew connection "+conn.RemoteAddr().String())
         if err != nil {
-            fmt.Println("getConn():\terror when accepting new connection -> break\n"+err.Error())
-            break
+            fmt.Println("getConn():\terror when accepting new connection -> continue\n"+err.Error())
+            continue
+        } else {
+            fmt.Println("getConn():\tnew connection "+conn.RemoteAddr().String())
         }
 
         // 1) we update the user map
@@ -62,49 +63,37 @@ func getConn(serverName *string, listener net.Listener, userMap map[net.Conn]*Cl
         fmt.Println("getConn():\tsend message 'TCCHAT_WELCOME\t"+ *serverName+"' to <"+userMap[conn].name+">")
         _ , firstMessageErr := conn.Write([]byte("TCCHAT_WELCOME\t"+ *serverName +"\n"))
         if firstMessageErr != nil {
-            fmt.Println("getConn():\terror when sending TCCHAT_WELCOME to <"+userMap[conn].name+"> -> break\n"+firstMessageErr.Error())
-            break
+            fmt.Println("getConn():\terror when sending TCCHAT_WELCOME to <"+userMap[conn].name+"> -> continue\n"+firstMessageErr.Error())
+            continue
         }
 
-        // 3) we listen to him, because he's supposed to send REGISTER right after connecting
+        // 3) we listen to it, because it's supposed to send REGISTER right after connecting
         go getMsg(serverName, conn, userMap,mutex)
     }
 }
 
 
 func getMsg(serverName *string, conn net.Conn, userMap map[net.Conn]*Client, mutex *sync.Mutex) {
-    fmt.Println("\ngetMsg():\tlistening on messages from <"+userMap[conn].name+">")
+    var userName string = userMap[conn].name
     var msgPieces []string
     var wholeMessage string
     reader := bufio.NewReader(conn)
 
+    fmt.Println("getMsg():\tlistening on messages from <"+userName+">")
     for {
         // receiving and parsing the new message
         text, err := reader.ReadString('\n')
         if err != nil {
-            fmt.Println("getMsg():\terror on reading message from <"+userMap[conn].name+"> ("+err.Error()+") -> force disconnect")
-            go sendToAll(userMap, "TCCHAT_USEROUT\t"+ userMap[conn].name +"\n")
-
-
-            fmt.Println("getMsg():\tUSEROUT\t"+ userMap[conn].name +" sent to all")
-            //mutex.Lock()
-            _, ok := userMap[conn]
-            //mutex.Unlock()
-            if ok {
-				mutex.Lock()
-                delete(userMap, conn)
-                mutex.Unlock()
-            } else {
-                fmt.Println("getMsg():\terror on disconnecting <"+userMap[conn].name+"> ("+err.Error()+") -> break")
-                break
-            }
-            fmt.Println("getMsg():\tuserMap updated -> "+map2string(userMap))
+            fmt.Println("getMsg():\terror on reading message from <"+userName+"> ("+err.Error()+") -> force disconnect")
+            disconnect(conn, userName, userMap, mutex)
+            return
         }
+
         text = strings.TrimSuffix(text, "\n")
         msgPieces = strings.SplitN(text, "\t", 4)
         if len(msgPieces) < 1 || msgPieces[0] == "" {
-            fmt.Println("getMsg():\terror on parsing message from <"+userMap[conn].name+"> -> break")
-            break
+            fmt.Println("getMsg():\terror on parsing message from <"+userName+"> -> continue")
+            continue
         }
 
         switch msgPieces[0] {
@@ -112,103 +101,125 @@ func getMsg(serverName *string, conn net.Conn, userMap map[net.Conn]*Client, mut
         // upon REGISTER, we send USERIN to all clients
         case "TCCHAT_REGISTER":
             if len(msgPieces) != 2 || msgPieces[1] == "" {
-                fmt.Println("getMsg():\terror on parsing message from <"+userMap[conn].name+"> -> break")
-                break
+                fmt.Println("getMsg():\terror on parsing message (classed REGISTER) from <"+userName+"> -> continue")
+                continue
+            } else {
+                fmt.Println("getMsg():\tgot "+text+" from <"+userName+">")
             }
-            fmt.Println("getMsg():\tgot 'REGISTER\t"+msgPieces[1]+"' from <"+userMap[conn].name+">")
+
             mutex.Lock()
             userMap[conn].name = msgPieces[1]
+            userName = msgPieces[1]
             mutex.Unlock()
             fmt.Println("getMsg():\tuserMap updated -> "+map2string(userMap))
-            go sendToAll(userMap, "TCCHAT_USERIN\t"+ userMap[conn].name +"\n")
-            fmt.Println("getMsg():\tUSERIN\t"+ userMap[conn].name +" sent to all")
+
+            go sendToAll(userMap, mutex, "TCCHAT_USERIN\t"+ userName +"\n")
 
 
         // upon MESSAGE, we send BCAST to all clients
         case "TCCHAT_MESSAGE":
             if len(msgPieces) != 3 || msgPieces[1] == "" || msgPieces[2] == "" || len(msgPieces[2]) > 140 {
-                fmt.Println("getMsg():\terror on parsing message (classed MESSAGE) from <"+userMap[conn].name+" -> break")
-                break
+                fmt.Println("getMsg():\terror on parsing message (classed MESSAGE) from <"+userName+" -> continue")
+                continue
+            } else {
+                fmt.Println("getMsg():\tgot "+text+" from <"+userName+">")
             }
-            fmt.Println("getMsg():\tgot 'MESSAGE\t"+msgPieces[1]+"\t"+msgPieces[2]+"' from <"+userMap[conn].name+">")
-            go sendToAll(userMap, "TCCHAT_BCAST\t"+msgPieces[1]+"\t"+msgPieces[2]+"\n")
-            fmt.Println("getMsg():\tBCAST\t"+msgPieces[1]+"\t"+msgPieces[2]+" sent to all")
+
+            go sendToAll(userMap, mutex, "TCCHAT_BCAST\t"+msgPieces[1]+"\t"+msgPieces[2]+"\n")
 
 
         // upon PRIVATE, we send PERSONAL to particular client
         case "TCCHAT_PRIVATE":
             if len(msgPieces) != 4 || msgPieces[1] == "" || msgPieces[2] == "" || msgPieces[3] == "" || len(msgPieces[3]) > 140 {
-                fmt.Println("getMsg():\terror on parsing message (classed PRIVATE) from <"+userMap[conn].name+" -> break")
-                break
+                fmt.Println("getMsg():\terror on parsing message (classed PRIVATE) from <"+userName+" -> continue")
+                continue
+            } else {
+                fmt.Println("getMsg():\tgot "+text+" from <"+userName+">")
             }
-            fmt.Println("getMsg():\tgot 'PRIVATE\t"+msgPieces[1]+"\t"+msgPieces[2]+"\t"+msgPieces[3]+"' from <"+userMap[conn].name+">")
-            found := false
-            wholeMessage = "TCCHAT_PERSONAL\t"+msgPieces[1]+"\t"+msgPieces[3]+"\n"
-            for userConn, userClient := range userMap {
-                if userClient.name == msgPieces[2] {
-                    found = true
-                    _ , err := userConn.Write([]byte(wholeMessage))
-                    if err != nil {
-                        msgCut := strings.TrimSuffix(wholeMessage, "\n")
-                        fmt.Println("getMsg():\terror when sending '"+msgCut+"' to <"+msgPieces[2]+"> ("+err.Error()+")")
-                    } else {
-                        fmt.Println("getMsg():\tPERSONAL\t"+msgPieces[1]+"\t"+msgPieces[3]+" sent to <"+msgPieces[2]+">")
-                    }
-                    break
-                }
-            }
+
+            destConn, destName, found := findUser(msgPieces[2], userMap, mutex)
             if !found {
                 fmt.Println("getMsg():\tcan't deliver pm, user <"+msgPieces[2]+"> not found ! ")
+            } else {
+                wholeMessage = "TCCHAT_PERSONAL\t"+msgPieces[1]+"\t"+msgPieces[3]+"\n"
+                sendTo(destConn, destName, wholeMessage)
             }
 
 
-        // upon USERS, we send USERLIST \t client1 \r client2 \r client3 \n
+        // upon USERS, we send TCCHAT_USERLIST \t client1 \r client2 \r client3 \n
         case "TCCHAT_USERS":
-            fmt.Println("getMsg():\tUSERS from <"+userMap[conn].name+">")
+            fmt.Println("getMsg():\tgot USERS from <"+userName+">")
             wholeMessage = "TCCHAT_USERLIST\t"+giveUsers(userMap)+"\n"
-            _ , err := conn.Write([]byte(wholeMessage))
-            if err != nil {
-                msgCut := strings.TrimSuffix(wholeMessage, "\n")
-                fmt.Println("getMsg():\terror when sending '"+msgCut+"' to <"+userMap[conn].name+"> ("+err.Error()+")")
-            } else {                    
-                fmt.Println("getMsg():\t"+wholeMessage+" sent to <"+userMap[conn].name+">")
-            }
+            sendTo(conn, userName, wholeMessage)
 
 
         // upon DISCONNECT, we delete the user from userMap and send USEROUT to all clients
         case "TCCHAT_DISCONNECT":
-            fmt.Println("getMsg():\tDISCONNECT from <"+userMap[conn].name+">")
-            go sendToAll(userMap, "TCCHAT_USEROUT\t"+ userMap[conn].name +"\n")
-            fmt.Println("getMsg():\tUSEROUT\t"+ userMap[conn].name +" sent to all")
-            //mutex.Lock()
-            _, ok := userMap[conn]
-            //mutex.Unlock()
-            if ok {
-				mutex.Lock()
-                delete(userMap, conn)
-                mutex.Unlock()
-            }
-            fmt.Println("getMsg():\tuserMap updated -> "+map2string(userMap))
+            fmt.Println("getMsg():\tgot DISCONNECT from <"+userName+">")
+            disconnect(conn, userName, userMap, mutex)
+            return
         }
     }
 }
 
-func sendToAll(userMap map[net.Conn]*Client, wholeMessage string) {
+func sendTo(conn net.Conn, userName string, wholeMessage string) {
+    messageCut := strings.TrimSuffix(wholeMessage, "\n")
+
+    _ , err := conn.Write([]byte(wholeMessage))
+    if err != nil {
+        fmt.Println("sendTo():\terror when sending '"+messageCut+"' to <"+userName+"> ("+err.Error()+")")
+    } else {
+        fmt.Println("sendTo():\t'"+messageCut+"' successfully sent to <"+userName+">")
+    }
+}
+
+func sendToAll(userMap map[net.Conn]*Client, mutex *sync.Mutex, wholeMessage string) {
+    ok := true
+    messageCut := strings.TrimSuffix(wholeMessage, "\n")
+
+    mutex.Lock()
     for userConn, userClient := range userMap {
         _ , err := userConn.Write([]byte(wholeMessage))
         if err != nil {
-            msgCut := strings.TrimSuffix(wholeMessage, "\n")
-            fmt.Println("sendToAll():\terror when sending '"+msgCut+"' to <"+userClient.name+"> : "+err.Error())
+            ok = false
+            fmt.Println("sendToAll():\terror when sending '"+messageCut+"' to <"+userClient.name+"> ("+err.Error()+")")
         }
+    }
+    mutex.Unlock()
+    if ok {
+        fmt.Println("sendToAll():\t'"+messageCut+"' successfully sent to all users.")
     }
 }
 
-func map2string(userMap map[net.Conn]*Client) string {
-    str := "[ "
-    for key, val := range userMap {
-        str += key.RemoteAddr().String()+":"+val.name+", "
+func disconnect(conn net.Conn, userName string, userMap map[net.Conn]*Client, mutex *sync.Mutex) {
+    go sendToAll(userMap, mutex, "TCCHAT_USEROUT\t"+ userName +"\n")
+
+    mutex.Lock()
+    _, ok := userMap[conn]
+    mutex.Unlock()
+    if ok {
+        mutex.Lock()
+        delete(userMap, conn)
+        mutex.Unlock()
+        fmt.Println("disconnect():\tsuccessfully disconnected <"+userName+">")
+    } else {
+        fmt.Println("disconnect():\terror on disconnecting <"+userName+">")
+        fmt.Println("disconnect():\tuserMap is now "+map2string(userMap))
     }
-    return str + " ]"
+}
+
+func findUser(userName string, userMap map[net.Conn]*Client, mutex *sync.Mutex) (net.Conn, string, bool) {
+    mutex.Lock()
+
+    for userConn, userClient := range userMap {
+        if userClient.name == userName {
+            mutex.Unlock()
+            return userConn, userName, true
+        }
+    }
+
+    mutex.Unlock()
+    return nil, "", false
 }
 
 func giveUsers (userMap map[net.Conn]*Client) string {
@@ -217,4 +228,12 @@ func giveUsers (userMap map[net.Conn]*Client) string {
         str += val.name + "\r"
     }
     return strings.TrimSuffix(str, "\r")
+}
+
+func map2string(userMap map[net.Conn]*Client) string {
+    var str string = "[ "
+    for key, val := range userMap {
+        str += key.RemoteAddr().String()+":"+val.name+", "
+    }
+    return str + " ]"
 }
